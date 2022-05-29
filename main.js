301b893a34d346a48698e14b92558b5fbf56cb36
kadattack <jn6091@student.uni-lj.si>
2022-05-29 15:43:13 +0200

    last

---
+++ main.js
@@ -0,0 +1,8 @@
+// $('.image-description').width($('.content-image').outerWidth())
+// console.log($('.content-image').outerWidth())
+// console.log($('.list-image-collection').children()[0])
+
+
+// $('.gallerybox').css("height","300px")
+// $('.gallerybox > a > img').css("height","300px")
+// $('.gallerybox > a > p').css("width", $('.gallerybox > a > p').width())
